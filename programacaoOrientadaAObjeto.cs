4daf9255ee0dd62cb447381ce271df790cfbfac0
João Augusto Cardoso dos Reis da Silva <132208368+Pontuego@users.noreply.github.com>
2025-05-03 23:27:22 -0300

    Create programacaoOrientadaAObjeto.cs

---
+++ programacaoOrientadaAObjeto.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace MyApp
+{
+    // Define uma classe chamada 'Aluno'
+    public class Aluno
+    {
+        public string nome = "João";
+        public int nota_Anual = 8;
+        public int cpf = 000111222;
+    }
+    // Classe principal para executar o programa
+    internal class POO
+    {
+        static void Main(string[] args)
+        {
+            // Programção orientada a objetos - POO
+            // Classes
+
+            Aluno aluno = new Aluno(); // Cria um objeto (instância) da classe Aluno
+
+            Console.WriteLine(aluno.nome);
+            Console.WriteLine(aluno.nota_Anual);
+            Console.WriteLine(aluno.cpf);
+        }
+    }
+}
