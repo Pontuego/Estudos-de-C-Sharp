70f13e90aa254c1f083669f8fdcaf76c967ad821
João A. Cardoso dos Reis da Silva <132208368+Pontuego@users.noreply.github.com>
2025-05-07 12:13:52 -0300

    Update estruturaCondicional_4.cs

--- Parte_1/estruturaCondicional_4.cs
+++ Parte_1/estruturaCondicional_4.cs
@@ -1,1 +1,30 @@
 
+using System;
+
+namespace MyApp
+{
+    internal class ConsoleApp4
+    {
+        static void Main(string[] args)
+        {
+            // ||
+            // &&
+            // !
+
+            Console.WriteLine("Digite um número:\n");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            int numero_2 = 6;
+            bool resultado = false;
+
+            if (numero == 6 || numero >= 6)
+            {
+                Console.WriteLine("É verdade");
+            }
+            else
+            {
+                Console.WriteLine("É mentira");
+            }
+        }
+    }
+}
+
