29268b60553b5df4cfe44d061bddef5fa8654ff8
João A. Cardoso dos Reis da Silva <132208368+Pontuego@users.noreply.github.com>
2025-05-07 12:13:16 -0300

    Update estruturaCondicional_3.cs

--- Parte_1/estruturaCondicional_3.cs
+++ Parte_1/estruturaCondicional_3.cs
@@ -1,1 +1,30 @@
 
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Estrutura condicional
+            // Operadores de atribuição
+            // Operadores de comparação
+            // se = if
+
+            //Console.WriteLine("Digite um número: ");
+            //int numero = Convert.ToInt32(Console.ReadLine());
+            int numero = 8;
+
+            if (numero <= 8)
+            {
+                Console.WriteLine("Número correto!");  
+            } // else = senão
+            else
+            {
+                Console.WriteLine("Não pode ser 8!");
+            }
+        }
+    }
+}
+
