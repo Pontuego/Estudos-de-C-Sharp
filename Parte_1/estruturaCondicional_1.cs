df2d1688019ac9ffa0b331e62b900683cd31962a
João A. Cardoso dos Reis da Silva <132208368+Pontuego@users.noreply.github.com>
2025-05-07 12:10:21 -0300

    Update and rename estruturaCondicional_1 to estruturaCondicional_1.cs

---
+++ Parte_1/estruturaCondicional_1.cs
@@ -0,0 +1,30 @@
+
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 10;
+
+            float numero_2 = 2.5f;
+
+            string nome = "João";
+
+            char caractere = 'J';
+
+            Console.WriteLine(caractere);
+        }
+    }
+}
+
+// TipoDessaVariavel nomeParaEssaVariavel = atribuirUmValor;
+// int - armazena numeros inteiros. Ex: 10
+// float - armazena numeros quebrados. Ex: 10.5f
+// string - armazena texto. Ex: "olá, mundo"
+// char - armazena um caractere. Ex: l
+// bool - verdadeiro ou falso
+//  Console.WriteLine("Hello World!");
+
