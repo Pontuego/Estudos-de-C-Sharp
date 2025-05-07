712c79ecd5d15358f3b277494c6a5910be2abe17
João A. Cardoso dos Reis da Silva <132208368+Pontuego@users.noreply.github.com>
2025-05-07 12:12:30 -0300

    Update estruturaCondicional_2.cs

--- Parte_1/estruturaCondicional_2.cs
+++ Parte_1/estruturaCondicional_2.cs
@@ -1,1 +1,22 @@
 
+using System;
+
+namespace MyApp
+{
+    internal class ConsoleApp2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva um número: ");
+            int numero_1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Escreva outro número: ");
+            int numero_2 = Convert.ToInt32(Console.ReadLine());
+
+            int total = numero_1 / numero_2; 
+            
+            Console.WriteLine("O seu total é: \n" + total);
+        }
+    }
+}
+
